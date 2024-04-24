15c543e9f26d6e62a30d68736f110b807a8f974b
ayanesato <sato.ayane@genetec.co.jp>
2024-04-24 11:20:11 +0900

    〜47 画像を追加

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,8 +5,7 @@ void main() {
   runApp(
       const MaterialApp(
     home: Scaffold(
-      body:GradientContainer(Color.fromARGB(100, 5, 21, 94),
-          Color.fromARGB(152, 73, 114, 222)),
+      body:GradientContainer.purple(),
     ),
   ));
 }
