b7589cfcc1c30b8c0714e3e61322799feb18a7dd
ayanesato <sato.ayane@genetec.co.jp>
2024-04-30 17:57:17 +0900

    ~112

--- section5/lib/widgets/expenses.dart
+++ section5/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:section5/models/expense.dart';
 import 'package:section5/widgets/expenses_list.dart';
+import 'package:section5/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -30,7 +31,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
         context: context,
-        builder: (ctx) => const Text("Modal bottom sheet"),
+        builder: (ctx) => const NewExpense(),
     );
   }
 
---
+++ section5/lib/widgets/new_expense.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:developer';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _newExpenseState();
+  }
+}
+
+class _newExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController=TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+
+@override
+Widget build(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(16),
+    child: Column(
+      children: [
+        TextField(
+          controller: _titleController,
+          maxLength: 50,
+          decoration: const InputDecoration(
+            label: Text("Title"),
+          ),
+        ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            prefixText: "\$",
+            label: Text("Amount"),
+          ),
+        ),
+        Row(
+          children: [
+            TextButton(onPressed: (){
+              Navigator.pop(context);
+            },
+                child: const Text("cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                log(_titleController.text);
+                log(_amountController.text);
+              },
+              child: const Text("Save Expense"),
+            )
+          ],
+        )
+      ],
+    ),
+  );
+}}
