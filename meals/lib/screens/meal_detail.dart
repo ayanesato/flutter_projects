882b11d88572711d58bb6f033f2505c1b126f4a1
ayanesato <sato.ayane@genetec.co.jp>
2024-05-07 17:50:11 +0900

    ~195

--- meals/lib/screens/meal_detail.dart
+++ meals/lib/screens/meal_detail.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:meals/main.dart';
 import 'package:meals/data/meal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/provider/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    required this.onToggleFavorite,
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals=ref.watch(favoriteMealsProvider);
+
+    final isFavorite=favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
-          IconButton(onPressed: (){
-            onToggleFavorite(meal);
-          },
-            icon: const Icon(Icons.star),
+          IconButton(
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      wasAdded ? "Meal added as a favorite!" : "Meal removed"),
+                ),
+              );
+            },
+            icon: Icon(isFavorite ? Icons.star: Icons.star_border),
           )
         ],
       ),
@@ -58,21 +71,22 @@ class MealDetailsScreen extends StatelessWidget {
               "Steps",
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.bold,
                   ),
             ),
-            const SizedBox(height: 14,),
+            const SizedBox(
+              height: 14,
+            ),
             for (final step in meal.steps)
               Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 8),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
                 child: Text(
                   step,
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
                 ),
               ),
           ],
