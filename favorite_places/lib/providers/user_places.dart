edd64fd6de6dbab7d7749b98c8378d4ffd5de951
ayanesato <sato.ayane@genetec.co.jp>
2024-05-13 15:00:24 +0900

    ~259

---
+++ favorite_places/lib/providers/user_places.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/models/place.dart';
+import 'dart:io';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title, File image, PlaceLocation location) {
+    final newPlace = Place(title: title, image: image, location: location);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+        (ref) => UserPlacesNotifier());
